83cc1eb9c273e9ee1eaaba97c246140a206943ed
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-12 14:53:48 +0700

    update logo

--- daisy-web-application/src/components/header/header.js
+++ daisy-web-application/src/components/header/header.js
@@ -2,7 +2,6 @@ import CallIcon from '@mui/icons-material/Call';
 import { Button, Container, Stack, Typography } from '@mui/material';
 import React from "react";
 import './header.css';
-import './header.css';
 
 export default function Header() {
 
@@ -10,7 +9,7 @@ export default function Header() {
         <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200', position: 'fixed', top: 0, boxShadow: '0 4px 22px 0px #E5E3E3', backgroundColor: '#ffffff', width: '100%'}}>
         <Stack spacing={20} direction="row">
             <Stack spacing={5} direction="row" style={{ float: 'left' }}>
-                <img src={require('../../assets/images/app_logo.avif')} className="headerLogo" />
+                <img src={require('../../assets/images/logo.jpg')} className="headerLogo" />
                 <Button href="/categories">
                     <Typography variant="subtitle1" className="headerItemText">Categories</Typography>
                 </Button>
@@ -24,9 +23,9 @@ export default function Header() {
                     <Typography variant="subtitle1" className="headerItemText">Studio</Typography>
                 </Button>
             </Stack>
-            <Stack spacing={3} direction="row" style={{ float: 'right' }}>
+            <Stack spacing={3} direction="row">
                     <Button href="/contact">
-                        <CallIcon />
+                        <CallIcon style={{color: 'black'}} />
                     <Typography variant="subtitle1" className="headerItemText">1900 9090</Typography>
                 </Button>
                 <Button href="/login">
