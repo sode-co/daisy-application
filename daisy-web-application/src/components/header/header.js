d2b942ecfc6058ef7b9fde3eea8a8517298570bc
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-12 11:27:17 +0700

    add icon

--- daisy-web-application/src/components/header/header.js
+++ daisy-web-application/src/components/header/header.js
@@ -1,12 +1,13 @@
+import CallIcon from '@mui/icons-material/Call';
 import { Button, Container, Stack, Typography } from '@mui/material';
 import React from "react";
+import './header.css';
 import './header.css';
 
 export default function Header() {
 
     return (
-        <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200', position: 'fixed', top: 0, boxShadow: '0 3px 0px #ECE9E8'}}>
-
+        <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200', position: 'fixed', top: 0, boxShadow: '0 4px 22px 0px #E5E3E3', backgroundColor: '#ffffff', width: '100%'}}>
         <Stack spacing={20} direction="row">
             <Stack spacing={5} direction="row" style={{ float: 'left' }}>
                 <img src={require('../../assets/images/app_logo.avif')} className="headerLogo" />
@@ -24,8 +25,9 @@ export default function Header() {
                 </Button>
             </Stack>
             <Stack spacing={3} direction="row" style={{ float: 'right' }}>
-                <Button href="/contact">
-                    <Typography variant="subtitle1" className="headerItemText">Contact</Typography>
+                    <Button href="/contact">
+                        <CallIcon />
+                    <Typography variant="subtitle1" className="headerItemText">1900 9090</Typography>
                 </Button>
                 <Button href="/login">
                     <Typography variant="subtitle1" className="headerItemText">Login</Typography>
