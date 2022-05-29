6c8ea00ef3ca7bf66864ddb320d87ffba240bfd7
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-29 15:21:33 +0700

    done login w gg button

--- daisy_application/lib/pages/common/header.dart
+++ daisy_application/lib/pages/common/header.dart
@@ -1,3 +1,5 @@
+import 'package:daisy_application/pages/common/responsive.dart';
+import 'package:daisy_application/pages/common/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,9 +8,56 @@ class Header extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        title: Center(child: Image.asset('/images/logo.png', width: 150), widthFactor: 100,),
+        title: (Responsive.isDesktop(context)) ?
+                Row(children: <Widget>[
+                  Image.asset('/images/logo.png', width: 150),
+                    TextButton(child: const Text('Categories', style: Style.stringBold), onPressed: () {}),
+                    TextButton(child: const Text('How it works', style: Style.stringBold), onPressed: () {}),
+                    TextButton(child: const Text('Find a designer', style: Style.stringBold), onPressed: () {}),
+                  ]) :
+                Center(child: Image.asset('/images/logo.png', width: 150)),
         actions: <Widget>[
-          TextButton(child: Text("Login", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)), onPressed: () {})
+          if(Responsive.isDesktop(context))
+          TextButton(onPressed: () {  },
+          child: RichText(
+            text: const TextSpan(
+              children: [
+                WidgetSpan(child: Icon(Icons.phone, color: Colors.black)),
+                TextSpan(text: '1900 9999', style: Style.stringBold)
+              ]
+            )
+          )),
+          if(Responsive.isDesktop(context))
+          const SizedBox(width: 5),
+          InkWell(
+            onTap: () {},
+            child: Ink(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                  color: Color.fromARGB(255, 188, 188, 188),
+                  width: 0.5,
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    Image.asset('/images/google.png', width: 20),
+                    const SizedBox(width: 5),
+                    if(Responsive.isDesktop(context))
+                    const Text('Login in with FPT mail', style: Style.stringBold),
+                    if(!Responsive.isDesktop(context))
+                    const Text('Login', style: Style.stringBold),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          if(Responsive.isDesktop(context))
+          const Text('Login', style: TextStyle(color: Colors.white)),
           ],
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
@@ -16,5 +65,5 @@ class Header extends StatelessWidget with PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
---
+++ daisy_application/lib/pages/common/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  // Text
+  static const stringBold = TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black);
+  static const whiteStringBold = TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white);
+}
