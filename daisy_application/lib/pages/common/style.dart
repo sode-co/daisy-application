7f7c0ee8753761944a09558cc5c8d4a90f08ed1e
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-29 21:18:01 +0700

    done intro text

--- daisy_application/lib/pages/common/style.dart
+++ daisy_application/lib/pages/common/style.dart
@@ -3,5 +3,4 @@ import 'package:flutter/material.dart';
 class Style {
   // Text
   static const stringBold = TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black);
-  static const whiteStringBold = TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white);
 }
