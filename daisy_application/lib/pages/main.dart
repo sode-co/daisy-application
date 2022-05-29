db8f723ebf6ed00043b32b1b340e1fc59074512e
tiendang <tiendvlp@gmail.com>
2022-05-29 22:09:48 +0700

    fix compile error

--- daisy_application/lib/pages/main.dart
+++ daisy_application/lib/pages/main.dart
@@ -33,7 +33,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key)
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
