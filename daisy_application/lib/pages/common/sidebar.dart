5b3895b61cebb8615e1b08ef3afa4e41114b9829
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-29 16:49:10 +0700

    init sidebar

--- daisy_application/lib/pages/common/sidebar.dart
+++ daisy_application/lib/pages/common/sidebar.dart
@@ -1,18 +1,30 @@
+import 'package:daisy_application/pages/common/style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/material/icons.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-
 class Sidebar extends StatelessWidget {
   const Sidebar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[TextButton.icon(onPressed: () {}, icon: Icon(Icons.home), label: Text('Home'),)],
-    ));
+      width: 200,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10.0, top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('  Daisy', style: Style.stringBold),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.home, color: Colors.black), label: const Text('Home', style: TextStyle(color: Colors.black))),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.list, color: Colors.black), label: const Text('Browse categories', style: TextStyle(color: Colors.black))),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.lightbulb, color: Colors.black), label: const Text('How it work', style: TextStyle(color: Colors.black))),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.person_search, color: Colors.black), label: const Text('Find a designer', style: TextStyle(color: Colors.black))),
+            const Divider(),
+            const Divider(color: Colors.transparent),
+            const Text('  Support', style: Style.stringBold),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.phone, color: Colors.black), label: const Text('1900 8989', style: TextStyle(color: Colors.black))),
+            TextButton.icon(onPressed: () {}, icon: const Icon(Icons.help, color: Colors.black), label: const Text('Help center', style: TextStyle(color: Colors.black))),
+          ],
+        )
+      )
+    );
   }
 }
