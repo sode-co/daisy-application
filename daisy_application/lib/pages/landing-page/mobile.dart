33ff54b36e092b275eb8bf527b8c982f0a194c8e
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-30 00:04:17 +0700

    add AnimatedSwitcher

--- daisy_application/lib/pages/landing-page/mobile.dart
+++ daisy_application/lib/pages/landing-page/mobile.dart
@@ -10,12 +10,20 @@ class BodyLandingPageMobile extends StatefulWidget {
 
 class _BodyLandingMobileState extends State<BodyLandingPageMobile> {
   int colorIndex = 0;
+  List<int> colors = [0xFF5022B8, 0xFF1C1B1A, 0xFF0493DD, 0xFF1C1B1A,  0xFF009697];
+  List<String> images = [
+      '/images/intro/clothes.png',
+      '/images/intro/brand.png',
+      '/images/intro/packaging.png',
+      '/images/intro/logo.png',
+      '/images/intro/packaging2.png',
+    ];
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    List<int> colors = [0xFF5022B8, 0xFF0493DD, 0xFF1C1B1A, 0xFF009697];
-    Timer.periodic(const Duration(seconds: 15), (timer) {
+
+    Timer.periodic(const Duration(seconds: 10), (timer) {
       if (colorIndex == colors.length - 1) {
         setState(() => {colorIndex = 0});
       } else {
@@ -29,18 +37,42 @@ class _BodyLandingMobileState extends State<BodyLandingPageMobile> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: EdgeInsets.only(left: size.width * 0.035),
-              child: introText(Color(colors[colorIndex]))
-            ),
+                padding: EdgeInsets.only(left: size.width * 0.035),
+                child: introText(size.width, Color(colors[colorIndex]))),
             Divider(
                 thickness: 6,
                 indent: size.width * 0.05,
                 endIndent: size.width * 0.85,
                 color: Color(colors[colorIndex])),
             const SizedBox(height: 20),
-            searchCategoriesTextField(size.width * 0.89, Color(colors[colorIndex])),
+            searchCategoriesTextField(
+                size.width * 0.89, Color(colors[colorIndex])),
+            renderImageCarousel()
           ],
         ));
+  }
+
+  Column renderImageCarousel() {
+    return Column(children: <Widget>[
+      AnimatedSwitcher(
+            duration: const Duration(milliseconds: 1000),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return ScaleTransition(scale: animation, child: child);
+            },
+            child:
+            SizedBox(
+            width: 500,
+            height: 400,
+            child: Image.asset(images[colorIndex], width: 450)),
+      ),
+      Row(children: [
+        IconButton(icon: const Icon(Icons.circle, color: Color.fromARGB(255, 190, 188, 188), size: 10), onPressed: () { setState(() => colorIndex = 0); },),
+        IconButton(icon: const Icon(Icons.circle, color: Color.fromARGB(255, 190, 188, 188), size: 10), onPressed: () { setState(() => colorIndex = 1); },),
+        IconButton(icon: const Icon(Icons.circle, color: Color.fromARGB(255, 190, 188, 188), size: 10), onPressed: () { setState(() => colorIndex = 2); },),
+        IconButton(icon: const Icon(Icons.circle, color: Color.fromARGB(255, 190, 188, 188), size: 10), onPressed: () { setState(() => colorIndex = 3); },),
+        IconButton(icon: const Icon(Icons.circle, color: Color.fromARGB(255, 190, 188, 188), size: 10), onPressed: () { setState(() => colorIndex = 4); },)
+        ]),
+    ]);
   }
 
   Column searchCategoriesTextField(width, color) {
@@ -60,41 +92,43 @@ class _BodyLandingMobileState extends State<BodyLandingPageMobile> {
         height: 10,
       ),
       SizedBox(
-      height: 50,
-      width: width,
-      child:
-            TextButton(
-                onPressed: () {  },
-                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
-                child: const Center(child: Text('Get started', style: Style.whiteStringBold)),
-      )),
+          height: 50,
+          width: width,
+          child: TextButton(
+            onPressed: () {},
+            style:
+                ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+            child: const Center(
+                child: Text('Get started', style: Style.whiteStringBold)),
+          )),
       const SizedBox(
         height: 10,
       ),
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
         SizedBox(width: width * 0.3),
-        Icon(Icons.play_circle, color: color), // sometimes it causes bug not in dom tree??? sometimes not
+        Icon(Icons.play_circle,
+            color:
+                color), // sometimes it causes bug not in dom tree??? sometimes not
         const SizedBox(width: 7),
-        Text('See creativity at work', style: TextStyle(color: color, fontWeight: FontWeight.w600)),
+        Text('See creativity at work',
+            style: TextStyle(color: color, fontWeight: FontWeight.w600)),
       ]),
     ]);
   }
 
-  Column introText(color) =>
+  Column introText(pageWidth, color) =>
       Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Text('World-class design',
             style: TextStyle(
               color: color,
               fontWeight: FontWeight.w900,
-              fontSize: 50,
+              fontSize: pageWidth * 0.1,
             )),
         Text('At your service',
             style: TextStyle(
               color: color,
               fontWeight: FontWeight.w900,
-              fontSize: 50,
+              fontSize: pageWidth * 0.1,
             )),
       ]);
 }
