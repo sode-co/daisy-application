eaf29ba4103322434bb5e36f4f2ee2ae74ae6794
= <54256540+ngocthieuff@users.noreply.github.com>
2022-05-31 19:07:20 +0700

    done intro page

--- daisy_application/lib/pages/landing-page/web.dart
+++ daisy_application/lib/pages/landing-page/web.dart
@@ -103,6 +103,12 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
               Padding(
                   padding: EdgeInsets.only(left: size.width * 0.35),
                   child: renderWelcomeToLogin()),
+              Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: size.width * 0.01,
+                      vertical: size.width * 0.05),
+                  child: renderSeeAllCategories(size.width)),
+              const SizedBox(height: 10),
             ],
           ),
         ),
@@ -111,8 +117,55 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
               const BoxDecoration(color: Color.fromARGB(255, 220, 220, 220)),
           child: Padding(
             padding: EdgeInsets.symmetric(
-                vertical: size.width * 0.05, horizontal: size.width * 0.1),
-            child: renderFooter(),
+                vertical: size.width * 0.05, horizontal: size.width * 0.05),
+            child: renderFooter(size.width),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row renderSeeAllCategories(pageWidth) {
+    return Row(
+      children: [
+        SizedBox(
+          width: pageWidth * 0.6,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Logos, websites, book covers & more!',
+                  style: Style.h5Bold),
+              Divider(
+                height: 5,
+                thickness: 4,
+                endIndent: pageWidth * 0.8,
+                color: Colors.black,
+              ),
+              SizedBox(
+                width: pageWidth * 0.35,
+                child: const Text(
+                    'Get the perfect logo design - or any design in over 90 categories! Whatever your business need or budget, we’ll help get it done.'),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 50,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black,
+            border: Border.all(color: Colors.black, style: BorderStyle.solid),
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: const Text(
+              'See all categories',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
           ),
         ),
       ],
@@ -120,45 +173,125 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
   }
 
   Row renderSocialMediaButton() {
-    return Row(children: [
-      IconButton(
-          onPressed: () {},
-          icon: const FaIcon(FontAwesomeIcons.facebook, size: 20)),
-      IconButton(
-          onPressed: () {},
-          icon: const FaIcon(FontAwesomeIcons.instagram, size: 20)),
-      IconButton(
-          onPressed: () {},
-          icon: const FaIcon(FontAwesomeIcons.linkedin, size: 20)),
-      IconButton(
-          onPressed: () {},
-          icon: const FaIcon(FontAwesomeIcons.twitter, size: 20)),
-    ]);
+    return Row(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.facebook, size: 20)),
+        IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.instagram, size: 20)),
+        IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.linkedin, size: 20)),
+        IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.twitter, size: 20)),
+      ],
+    );
   }
 
   Row renderCopyright() {
-    return Row(children: const [
-      Text('© Daisy'),
-      Text(' | '),
-      Text('by Sode'),
-      Text(' | '),
-      Text('Term'),
-      Text(' | '),
-      Text('Privacy'),
-      Text('  '),
-      Icon(Icons.public),
-      Text('English'),
+    return Row(
+      children: const [
+        Text('© Daisy'),
+        Text(' | '),
+        Text('by Sode'),
+        Text(' | '),
+        Text('Term'),
+        Text(' | '),
+        Text('Privacy'),
+        Text('  '),
+        Icon(Icons.public),
+        Text('English'),
+      ],
+    );
+  }
+
+  Row renderResourceFooter() {
+    return Row(children: [
+      SizedBox(
+        width: 500,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/weblogo.png', width: 300),
+            Row(
+              children: [
+                Image.asset('assets/images/appstore.png', scale: 3),
+                Image.asset('assets/images/chplay.png', scale: 4),
+              ],
+            ),
+            const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula efficitur nibh non commodo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In nibh neque, commodo eget dui ac, hendrerit commodo risus. In lobortis rutrum velit, eget efficitur nibh laoreet vitae. Duis vel elit mollis nulla tincidunt auctor at in justo. Sed viverra diam arcu, eu elementum quam porttitor commodo.'),
+          ],
+        ),
+      ),
+      const SizedBox(
+        width: 120,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Company', style: Style.stringBold),
+          Text('About'),
+          Text('Contact'),
+          Text('Careers'),
+          Text('Team'),
+          Text('Press releases'),
+          Text('In the media'),
+          Text('Testimonials'),
+        ],
+      ),
+      const SizedBox(
+        width: 120,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Design services', style: Style.stringBold),
+          Text('Find a designer'),
+          Text('Discover inspiration'),
+          Text('Pricing'),
+          Text('Agencies'),
+          Text('Designer resources'),
+          Text('Featured partners'),
+          Text('Help'),
+        ],
+      ),
+      const SizedBox(
+        width: 120,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Get a design', style: Style.stringBold),
+          Text('Logo design'),
+          Text('Business card'),
+          Text('Web page design'),
+          Text('Brand guide'),
+          Text('Packaging design'),
+          Text('T-shirt design'),
+          Text('Book cover design'),
+        ],
+      ),
     ]);
   }
 
-  Column renderFooter() {
+  Column renderFooter(pageWidth) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        renderCopyright(),
-        Padding(
-            padding: const EdgeInsets.only(left: 60),
-            child: renderSocialMediaButton()),
+        renderResourceFooter(),
+        Row(
+          children: [
+            SizedBox(width: pageWidth * 0.41),
+            renderCopyright(),
+            Padding(
+                padding: EdgeInsets.only(left: pageWidth * 0.05),
+                child: renderSocialMediaButton()),
+          ],
+        ),
       ],
     );
   }
@@ -222,13 +355,14 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
                 ),
               ),
               child: Center(
-                  child: Text(
-                '$i',
-                style: const TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w900,
-                    color: MyColor.orange),
-              )),
+                child: Text(
+                  '$i',
+                  style: const TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w900,
+                      color: MyColor.orange),
+                ),
+              ),
             );
           },
         );
@@ -242,8 +376,9 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
     double imgHeight = 150;
     imageItem.forEach((k, v) => {title = k, imgSrc = v});
     return SizedBox(
-        width: pageWidth * 0.1,
-        child: Column(children: [
+      width: pageWidth * 0.1,
+      child: Column(
+        children: [
           InkWell(
               onHover: (isHovering) {
                 if (isHovering) {
@@ -254,7 +389,9 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
               },
               child: Image.asset(imgSrc, height: imgHeight)),
           Text(title)
-        ]));
+        ],
+      ),
+    );
   }
 
   ListView trendingTab(pageWidth) {
@@ -267,115 +404,139 @@ class _BodyLandingWebState extends State<BodyLandingPageWeb> {
   }
 
   Column renderImageCarousel() {
-    return Column(children: <Widget>[
-      AnimatedSwitcher(
-        duration: const Duration(milliseconds: 1000),
-        transitionBuilder: (Widget child, Animation<double> animation) {
-          return ScaleTransition(scale: animation, child: child);
-        },
-        child: SizedBox(
-            width: 650,
-            height: 350,
-            child: Image.asset(images[colorIndex], width: 450)),
-      ),
-      Row(children: [
-        IconButton(
-          icon: const Icon(Icons.circle,
-              color: Color.fromARGB(255, 190, 188, 188), size: 10),
-          onPressed: () {
-            setColorIndex(0);
+    return Column(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 1000),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return ScaleTransition(scale: animation, child: child);
           },
+          child: SizedBox(
+              width: 650,
+              height: 350,
+              child: Image.asset(images[colorIndex], width: 450)),
         ),
-        IconButton(
-          icon: const Icon(Icons.circle,
-              color: Color.fromARGB(255, 190, 188, 188), size: 10),
-          onPressed: () {
-            setColorIndex(1);
-          },
+        Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.circle,
+                  color: Color.fromARGB(255, 190, 188, 188), size: 10),
+              onPressed: () {
+                setColorIndex(0);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.circle,
+                  color: Color.fromARGB(255, 190, 188, 188), size: 10),
+              onPressed: () {
+                setColorIndex(1);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.circle,
+                  color: Color.fromARGB(255, 190, 188, 188), size: 10),
+              onPressed: () {
+                setColorIndex(2);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.circle,
+                  color: Color.fromARGB(255, 190, 188, 188), size: 10),
+              onPressed: () {
+                setColorIndex(3);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.circle,
+                  color: Color.fromARGB(255, 190, 188, 188), size: 10),
+              onPressed: () {
+                setColorIndex(4);
+              },
+            ),
+          ],
         ),
-        IconButton(
-          icon: const Icon(Icons.circle,
-              color: Color.fromARGB(255, 190, 188, 188), size: 10),
-          onPressed: () {
-            setColorIndex(2);
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.circle,
-              color: Color.fromARGB(255, 190, 188, 188), size: 10),
-          onPressed: () {
-            setColorIndex(3);
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.circle,
-              color: Color.fromARGB(255, 190, 188, 188), size: 10),
-          onPressed: () {
-            setColorIndex(4);
-          },
-        )
-      ]),
-    ]);
+      ],
+    );
   }
 
   Column searchCategoriesTextField(width, color) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      SizedBox(
-        width: width,
-        child: const TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            prefixIcon: Icon(Icons.search),
-            labelText: 'Logo, website, branding...',
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: width,
+          child: const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.search),
+              labelText: 'Logo, website, branding...',
+            ),
           ),
         ),
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      SizedBox(
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
           height: 50,
           width: width,
           child: TextButton(
             onPressed: () {},
-            style:
-                ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(color),
+            ),
             child: const Center(
-                child: Text('Get started', style: Style.whiteStringBold)),
-          )),
-      const SizedBox(
-        height: 10,
-      ),
-      Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Icon(Icons.play_circle,
-            color:
-                color), // sometimes it causes bug not in dom tree??? sometimes not
-        const SizedBox(width: 7),
-        Text('See creativity at work',
-            style: TextStyle(color: color, fontWeight: FontWeight.w600)),
-      ]),
-    ]);
+              child: Text('Get started', style: Style.whiteStringBold),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(Icons.play_circle,
+                color:
+                    color), // sometimes it causes bug not in dom tree??? sometimes not
+            const SizedBox(width: 7),
+            Text(
+              'See creativity at work',
+              style: TextStyle(color: color, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
-  Column introText(pageWidth, color) =>
-      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text('World-class design',
+  Column introText(pageWidth, color) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'World-class design',
             style: TextStyle(
               color: color,
               fontWeight: FontWeight.w900,
               fontSize: pageWidth * 0.035,
-            )),
-        Text('At your service',
+            ),
+          ),
+          Text(
+            'At your service',
             style: TextStyle(
               color: color,
               fontWeight: FontWeight.w900,
               fontSize: pageWidth * 0.035,
-            )),
-        Divider(
+            ),
+          ),
+          Divider(
             thickness: 6,
             indent: pageWidth * 0.005,
             endIndent: pageWidth * 0.085,
-            color: Color(colors[colorIndex])),
-      ]);
+            color: Color(
+              colors[colorIndex],
+            ),
+          ),
+        ],
+      );
 }
