52bfc5aa13d3c54197e02aa8f9b87f6f535fbf6c
Nguyễn Văn Khang <nguyenvankhang0211@gmail.com>
2022-06-08 00:20:49 +0700

    fix conflict

--- daisy-core-services/Api/Controllers/UserController/UserController.cs
+++ daisy-core-services/Api/Controllers/UserController/UserController.cs
@@ -1,21 +1,20 @@
 ﻿using DataAccess.Repositories.Users;
 using DataAccess.UnitOfWork;
 using Domain.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Api.Controllers.UserController
+namespace Api.Controllers.CustomerController
 {
     [ApiController]
     [Route("v1/users")]
     public partial class UserController : Controller
     {
 
-        protected UnitOfWorkFactory _unitOfWorkFactory;
+        private UnitOfWorkFactory _unitOfWorkFactory;
         public UserController(UnitOfWorkFactory unitOfWorkFactory)
         {
             this._unitOfWorkFactory = unitOfWorkFactory;
@@ -24,6 +23,7 @@ namespace Api.Controllers.UserController
         [HttpGet("category/{id}")]
         public IEnumerable<User> GetDesignersByCategory(int id)
         {
+
             using (var work = _unitOfWorkFactory.Get)
             {
                 var designers = _unitOfWorkFactory.Get.UserRepository.GetDesignersByCategory(id);
@@ -33,7 +33,84 @@ namespace Api.Controllers.UserController
                 }
                 return designers;
             }
+            
+        }
 
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                var user = work.UserRepository.Get(id);
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                return user;
+            }   
+        }
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                var users = work.UserRepository.GetUsers();
+
+                return users;
+            }
+
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateUser(User user)
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                work.UserRepository.Add(user);
+
+                return Created(nameof(GetUser), user);
+            }
+        }
+
+        //[HttpPut("{id}")]
+        //public ActionResult UpdateUser(int id, User newUser)
+        //{
+        //    using (var work = _unitOfWorkFactory.Get)
+        //    {
+        //        var existingUser = work.UserRepository.Get(id);
+        //        if (existingUser is null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        User updateUser = existingUser with
+        //        {
+        //            FirstName = newUser.Name,
+        //            Price = newUser.Price
+        //        };
+
+        //        work.UserRepository.UpdateUser(updateUser);
+
+        //    }
+        //    return NoContent();
+        //}
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult DeleteItem(int id)
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                var deletedUser = work.UserRepository.Get(id);
+
+                if (deletedUser is null)
+                {
+                    return NotFound();
+                }
+
+                work.UserRepository.Remove(id);
+                return NoContent();
+            }               
         }
 
     }
