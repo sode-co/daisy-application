ce2bbe30d6cd6ae0fbc2e016f5f4f1fc8027578a
tiendang <tiendvlp@gmail.com>
2022-06-08 11:26:37 +0700

    support authorize on swagger

--- daisy-core-services/Api/Startup.cs
+++ daisy-core-services/Api/Startup.cs
@@ -78,7 +78,31 @@ namespace Api
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Daisy", Version = "v1.0.0" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                    {
+                        { securitySchema, new[] { "Bearer" } }
+                    };
+
+                c.AddSecurityRequirement(securityRequirement);
+
             });
 
             services.AddAuthentication().AddGoogle(googleOptions =>
