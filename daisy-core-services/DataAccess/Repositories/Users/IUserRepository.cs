03a6ef5608e3dce82753465a1bd054721069330e
Nguyễn Văn Khang <nguyenvankhang0211@gmail.com>
2022-06-14 19:52:54 +0700

    fix comment 2

--- daisy-core-services/DataAccess/Repositories/Users/IUserRepository.cs
+++ daisy-core-services/DataAccess/Repositories/Users/IUserRepository.cs
@@ -10,12 +10,11 @@ namespace DataAccess.Repositories.Users
 {
     public interface IUserRepository : IRepository<User>
     {
-        IEnumerable<User> GetDesignersByCategory(int categoryId);
-
-        void CreateUser(User user);
         User GetUser(int id);
         IEnumerable<User> GetUsers();
         IEnumerable<User> GetUsersByName(string name);
+        IEnumerable<User> GetDesignersByCategory(int categoryId);
+        void CreateUser(User user);
         void UpdateUser(User user);
         void DeleteUser(User user);
        
