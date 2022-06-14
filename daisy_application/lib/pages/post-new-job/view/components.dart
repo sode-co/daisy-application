001bb65af161d64c4231d132fb84076f8c09bbc1
= <54256540+ngocthieuff@users.noreply.github.com>
2022-06-14 19:22:33 +0700

    done parent frameUI

--- daisy_application/lib/pages/post-new-job/view/components.dart
+++ daisy_application/lib/pages/post-new-job/view/components.dart
@@ -28,18 +28,23 @@ class PostNewJobForm extends StatefulWidget {
 class _PostNewJobFormState extends State<PostNewJobForm> {
   String dropdownValue = 'One';
   List<String> categories = ['One', 'Two', 'Three', 'Four'];
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return SizedBox(
-        width: 800,
-        height: 3000,
+      width: 800,
+      height: 3000,
+      child: Form(
+        key: _formKey,
         child: Column(
           children: [
             Row(
-              verticalDirection: VerticalDirection.up,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Text('image'),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -47,27 +52,85 @@ class _PostNewJobFormState extends State<PostNewJobForm> {
                     renderDropdownList(
                         'Chọn lĩnh vực cần tuyển', categories, size),
                     renderTitleTextField(
-                        'Đặt tên cụ thể cho công việc bạn cần tuyển',
-                        'Tên dự án',
-                        size),
+                      'Đặt tên cụ thể cho công việc bạn cần tuyển',
+                      'Tên dự án',
+                      1,
+                      size,
+                    ),
                   ],
                 ),
               ],
             ),
-            Row(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('image'),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.8,
+                      child: Flexible(
+                        child: Text('Thông tin đầy đủ về yêu cầu tuyển dụng',
+                            style: Style.h6Bold),
+                      ),
+                    ),
+                    renderTitleTextField(
+                      'Nội dung chi tiết, và các đầu việc cần Designer thực hiện (càng chi tiết designer càng có đầy đủ thông tin để hoàn thiện sản phẩm)',
+                      'Mô tả dự án',
+                      12,
+                      size,
+                    ),
+                    renderTitleTextField(
+                      'Ngân sách dự án',
+                      'Ngân sách',
+                      1,
+                      size,
+                      (value) {
+                        if (value == null) {
+                          return 'Giá trị nhập vào phải là số thập phân???';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 
-  Column renderTitleTextField(String fieldName, String label, Size size) {
+  Column renderTitleTextField(
+      String fieldName, String label, int maxLines, Size size,
+      [String? Function(String?)? validation]) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(fieldName, style: Style.stringText),
+        SizedBox(
+            width: size.width * 0.8,
+            child: Flexible(child: Text(fieldName, style: Style.stringText))),
         SizedBox(
           width: size.width * 0.8,
           height: 100,
           child: TextFormField(
+            validator: validation,
+            maxLines: maxLines,
             decoration: InputDecoration(
               border: const OutlineInputBorder(),
               labelText: label,
