3f00b6e351e783bfb79657798f4e96215b736ce1
Triet0211 <triet02112001@gmail.com>
2022-06-17 12:43:10 +0700

    Update category model and db

---
+++ daisy-core-services/DataAccess/MssqlServerIntegration/Migrations/20220617053257_UpdateCategories.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.MssqlServerIntegration.Migrations
+{
+    public partial class UpdateCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryCategory",
+                columns: table => new
+                {
+                    ChildrenCategoryId = table.Column<int>(type: "int", nullable: false),
+                    ParentCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryCategory", x => new { x.ChildrenCategoryId, x.ParentCategoryId });
+                    table.ForeignKey(
+                        name: "FK_CategoryCategory_Categories_ChildrenCategoryId",
+                        column: x => x.ChildrenCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryCategory_Categories_ParentCategoryId",
+                        column: x => x.ParentCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryCategory_ParentCategoryId",
+                table: "CategoryCategory",
+                column: "ParentCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryCategory");
+        }
+    }
+}
