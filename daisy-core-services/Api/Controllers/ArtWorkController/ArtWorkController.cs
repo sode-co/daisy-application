2f25eff906d910bfa061c7e0d1b9d87a39599c53
HoangDuong1106 <79978327+HoangDuong1106@users.noreply.github.com>
2022-06-20 01:42:03 -0700

    [SD-3b030] update art work by & change API name to v1/users/{id} &  [SD-dd36b] Get Artwork Detail by Id (#73)
    * [Dan] change to Get v1/user/{id}
    
    * [Dan] Implement Get Artwork Detail By Id
    
    * [Dan] Implement Update Artwork By Id
    
    * Update daisy-core-services/Api/Controllers/ArtWorkController/ArtWorkController.cs
    
    Co-authored-by: Dang Minh Tien <tiendvlp@gmail.com>
    
    * Update daisy-core-services/Api/Controllers/ArtWorkController/ArtWorkController.cs
    
    Co-authored-by: Dang Minh Tien <tiendvlp@gmail.com>
    
    * fix controller
    
    Co-authored-by: Duong <maihoangduong@gmail.com>
    Co-authored-by: Dang Minh Tien <tiendvlp@gmail.com>
    Co-authored-by: Nguyễn Văn Khang <nguyenvankhang0211@gmail.com>

--- daisy-core-services/Api/Controllers/ArtWorkController/ArtWorkController.cs
+++ daisy-core-services/Api/Controllers/ArtWorkController/ArtWorkController.cs
@@ -3,6 +3,7 @@ using Domain.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Utils.Models;
 using static Api.Common.Constants;
 
 namespace Api.Controllers.ArtWorkController
@@ -19,7 +20,20 @@ namespace Api.Controllers.ArtWorkController
             this._unitOfWorkFactory = unitOfWorkFactory;
         }
 
+        [HttpGet("{artworkId}")]
+        public IActionResult GetArtworkById(int artworkId)
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                ArtWork artWork = work.ArtWorkRepository.Get(artworkId);
+                if (artWork != null) return Json(artWork);
+
+                return NotFound();
+            }
+        }
+
         [HttpGet("category/{id}")]
+        [Authorize(Policy = ROLE.DESIGNER)]
         public IEnumerable<ArtWork> GetArtWorksByCategory(int id)
         {
             using (var work = _unitOfWorkFactory.Get)
@@ -42,12 +56,31 @@ namespace Api.Controllers.ArtWorkController
             {
                 ArtWork artWork = work.ArtWorkRepository.GetFirstOrDefault(wa => wa.Id == id);
                 if (artWork is null) return NotFound();
-                
+
                 artWork.DeletedAt = System.DateTime.Now;
                 work.Save();
                 return Json(new { message = "ok" });
             }
         }
+
+        [HttpPut("{artworkId}")]
+        [Authorize(Policy = ROLE.DESIGNER)]
+        public IActionResult UpdateArtWorkById(int artworkId, [FromBody] ArtWorkVM artWorkVM)
+        {
+            using (var work = _unitOfWorkFactory.Get)
+            {
+                ArtWork artWork = work.ArtWorkRepository.Get(artworkId);
+                if (artWork is null) return NotFound();               
+                artWork.Description = artWorkVM.Description;
+                artWork.Title = artWorkVM.Title;
+                artWork.Category = work.CategoryRepository.Get(artWorkVM.CategoryId);
+                work.Save();
+
+                return Json(new { message = "ok" });
+            }
+            
+        }
+
     }
 
 }
