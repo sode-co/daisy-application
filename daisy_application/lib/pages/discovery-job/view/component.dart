c886200c44d34c5dd3fe787c00012a0da81e9970
= <54256540+ngocthieuff@users.noreply.github.com>
2022-06-20 17:22:00 +0700

    done job request lines

--- daisy_application/lib/pages/discovery-job/view/component.dart
+++ daisy_application/lib/pages/discovery-job/view/component.dart
@@ -41,9 +41,59 @@ class _JobInfoColumnState extends State<JobInfoColumn> {
   );
   @override
   Widget build(BuildContext context) {
+    return const RequestIntroList();
+  }
+}
+
+class RequestIntroList extends StatelessWidget {
+  const RequestIntroList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CategoryModel category = CategoryModel(
+      1,
+      'category description',
+      'Logo',
+      'type',
+    );
+    UserModel user = UserModel(
+      1,
+      'firstname',
+      'lastname',
+      'Select Technology, LLC ',
+      'email',
+      'role',
+      'setting',
+      'assets/images/portfolio/avatar.png',
+      'District 7, Ho Chi Minh City, Vietnam ',
+      'phone',
+    );
+    late RequestModel request = RequestModel(
+      1,
+      user,
+      category,
+      'Mobile Application Developer (iOS or/and Android)',
+      '1',
+      1.0,
+      '1',
+    );
     return SingleChildScrollView(
       child: Column(
         children: [
+          IntroJobCard(
+            img: 'assets/images/portfolio/avatar.png',
+            request: request,
+          ),
+          IntroJobCard(
+            img: 'assets/images/portfolio/avatar.png',
+            request: request,
+          ),
+          IntroJobCard(
+            img: 'assets/images/portfolio/avatar.png',
+            request: request,
+          ),
           IntroJobCard(
             img: 'assets/images/portfolio/avatar.png',
             request: request,
--- daisy_application/lib/pages/discovery-job/view/stateless_component.dart
+++ daisy_application/lib/pages/discovery-job/view/stateless_component.dart
@@ -10,89 +10,92 @@ class IntroJobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 80.0,
-            width: 80.0,
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                request.user!.avatar.toString(),
+    return InkWell(
+      onTap: () {},
+      child: SizedBox(
+        width: 400,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 80.0,
+              width: 80.0,
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  request.user!.avatar.toString(),
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 10.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 250,
-                child: Text(
-                  request.title.toString(),
-                  style: const TextStyle(
-                    color: Color(BuiltinColor.blue_gradient_01),
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w600,
+            const SizedBox(width: 10.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                    request.title.toString(),
+                    style: const TextStyle(
+                      color: Color(BuiltinColor.blue_gradient_01),
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 5.0,
-              ),
-              SizedBox(
-                width: 250,
-                child: Text(
-                  request.user!.displayName!,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.7),
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+                const SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              SizedBox(
-                width: 250,
-                child: Text(
-                  request.user!.address!,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.7),
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              Row(
-                children: [
-                  const Icon(Icons.explore, color: Colors.green),
-                  const SizedBox(
-                    width: 2.0,
-                  ),
-                  Text(
-                    'Đang tuyển',
+                SizedBox(
+                  width: 280,
+                  child: Text(
+                    request.user!.displayName!,
                     style: TextStyle(
                       color: Colors.black.withOpacity(0.7),
                       fontSize: 15.0,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 5.0,
-              ),
-              Container(
-                color: Colors.grey.withOpacity(0.3),
-                child: const SizedBox(width: 300, height: 1.0),
-              ),
-            ],
-          ),
-        ],
+                ),
+                SizedBox(
+                  width: 280,
+                  child: Text(
+                    request.user!.address!,
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.explore, color: Colors.green),
+                    const SizedBox(
+                      width: 2.0,
+                    ),
+                    Text(
+                      'Đang tuyển',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.7),
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 2.0,
+                ),
+                Container(
+                  color: Colors.grey.withOpacity(0.3),
+                  child: const SizedBox(width: 300, height: 1.0),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
