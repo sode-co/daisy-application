a32ad8b45f81afb1fd87544a0c99dccd3baaf520
= <54256540+ngocthieuff@users.noreply.github.com>
2022-06-28 16:32:16 +0700

    add category

--- daisy-core-services/WebApplication/Startup.cs
+++ daisy-core-services/WebApplication/Startup.cs
@@ -1,3 +1,4 @@
+using DataAccess.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<UnitOfWorkFactory>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
