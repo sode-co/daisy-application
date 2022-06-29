9736a080bfcd22eb2c68a7a698e6a697255ded83
= <54256540+ngocthieuff@users.noreply.github.com>
2022-06-29 23:37:42 +0700

    add user update form ui

--- daisy_application/lib/app/pages/update-profile/model/update_profile_state.dart
+++ daisy_application/lib/app/pages/update-profile/model/update_profile_state.dart
@@ -2,7 +2,7 @@ import 'package:daisy_application/core_services/models/user/user_model.dart';
 import 'package:flutter/material.dart';
 
 class UpdateProfileState with ChangeNotifier {
-  UserModel user = UserModel.init()
+  UserModel userProfile = UserModel.init()
     ..displayName = 'Ngoc'
     ..id = 1;
 }
--- daisy_application/lib/app/pages/update-profile/view/update_profile.dart
+++ daisy_application/lib/app/pages/update-profile/view/update_profile.dart
@@ -3,6 +3,7 @@ import 'package:daisy_application/app/common/widget/bottom_nav/bottomnavbar.dart
 import 'package:daisy_application/app/common/widget/header/header.dart';
 import 'package:daisy_application/app/listeners/WidgetListener.dart';
 import 'package:daisy_application/app/pages/update-profile/deps/update_profile_deps.dart';
+import 'package:daisy_application/app/pages/update-profile/view/component.dart';
 import 'package:flutter/material.dart';
 
 class UpdateProfile extends StatefulWidget {
@@ -19,9 +20,23 @@ class _UpdateProfileState extends State<UpdateProfile> with WidgetListener {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: const Header(),
-      body: const Text('Update Profile'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: size.width * 0.1, right: size.width * 0.1, top: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              UpdateProfileTitle(),
+              UpdateProfileForm(),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar:
           !Responsive.isDesktop(context) ? const BottomNavBar() : null,
     );
