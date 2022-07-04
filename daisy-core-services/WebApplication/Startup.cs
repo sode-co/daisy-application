3a2639c5ee38d4b316ec82743b000832effef44c
baolongt <tranbaolong14@gmail.com>
2022-07-04 22:52:23 +0700

    [SD-f8fff] Add front-end CRUD User

--- daisy-core-services/WebApplication/Startup.cs
+++ daisy-core-services/WebApplication/Startup.cs
@@ -4,10 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataAccess.MssqlServerIntegration;
 
 namespace WebApplication
 {
@@ -23,6 +20,7 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>();
             services.AddRazorPages();
         }
 
