9ec0aaa0d98da3f562a664b9b6972d07fae076dc
Ngoc Thieu <54256540+ngocthieuff@users.noreply.github.com>
2022-07-06 16:58:16 +0700

    [PRN][SD-70921] Refactor CRUD User (#109)
    use user repository

---
+++ daisy-core-services/WebApplication/Pages/Areas/Admin/User/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.MssqlServerIntegration;
+using Domain.Models;
+using WebApplication.Pages.Utils;
+using DataAccess.UnitOfWork;
+using SaleWebApp.Paging;
+
+namespace WebApplication.Pages.UserCRUD
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int CurrentPage { get; set; }
+        public int ItemPerPage { get; set; }
+
+        private UnitOfWorkFactory _unitOfWorkFactory;
+        public IndexModel(UnitOfWorkFactory unitOfWorkFactory)
+        {
+            this._unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public IList<User> User { get;set; }
+        
+        public decimal TotalPage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int p = 1)
+        {
+            string role = UserAuthentication.Role();
+
+            if (role != "ADMIN")
+            {
+                return Redirect("/Unauthorized");
+            }
+
+            ItemPerPage = 10;
+            CurrentPage = p;
+
+            var users = _unitOfWorkFactory.Get.UserRepository.GetUsers();
+            
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                users = _unitOfWorkFactory.Get.UserRepository.GetUsersByName(SearchString);
+            }
+            decimal tmp = Math.Ceiling(Convert.ToDecimal(users.Count()/ ItemPerPage));
+            TotalPage = tmp == 0 ? 1 : tmp;
+            User = users.Skip((CurrentPage - 1) * ItemPerPage).Take(ItemPerPage).ToList();
+            
+            return Page();
+        }
+    }
+}
