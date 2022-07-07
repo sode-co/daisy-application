430790d0dd86f3b92ae9606af5d569268fdb34fa
= <54256540+ngocthieuff@users.noreply.github.com>
2022-07-07 13:36:12 +0700

    done get list applicants

---
+++ daisy-core-services/WebApplication/Pages/Areas/Customers/Applications/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.MssqlServerIntegration;
+using Domain.Models;
+using WebApplication.Pages.Utils;
+using DataAccess.UnitOfWork;
+using SaleWebApp.Paging;
+
+namespace WebApplication.Pages.Areas.Customers.Applications
+{
+    public class IndexModel : PageModel
+    {
+        private UnitOfWorkFactory _unitOfWorkFactory;
+        public IndexModel(UnitOfWorkFactory unitOfWorkFactory)
+        {
+            this._unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public PaginatedList<JobApplication> JobApplication { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? pageIndex, int? requestId)
+        {
+            string role = UserAuthentication.Role();
+
+            if (role != "CUSTOMER")
+            {
+                return Redirect("/Unauthorized");
+            }
+
+            IQueryable<JobApplication> jobApplications;
+                jobApplications = _unitOfWorkFactory.Get.JobApplicationRepository.GetAll(jobApplication => jobApplication.Request.Id == requestId && jobApplication.DeletedAt == null).Include(jobApplication => jobApplication.Freelancer);
+
+            JobApplication = await PaginatedList<JobApplication>.CreateAsync(
+                jobApplications.AsNoTracking(), pageIndex ?? 1, 10);
+
+            return Page();
+        }
+    }
+}
