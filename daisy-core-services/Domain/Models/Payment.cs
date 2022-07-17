fbf15af69e3159ac4602bceca948ec52c42b8fd3
Nguyễn Văn Khang <nguyenvankhang0211@gmail.com>
2022-07-17 16:59:51 +0700

    approve job app

--- daisy-core-services/Domain/Models/Payment.cs
+++ daisy-core-services/Domain/Models/Payment.cs
@@ -18,6 +18,8 @@ namespace Domain.Models
         [MaxLength(255)]
         public String Currency { get; set; }
 
+        public ICollection<PaymentAction> PaymentActions { get; set; }
+
         [MaxLength(int.MaxValue)]
         public String Data { get; set; } = "{}";
 
