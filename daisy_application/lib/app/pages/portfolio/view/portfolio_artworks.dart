63dd9762f3add75e98c8abdfcd27db1970400cd3
= <54256540+ngocthieuff@users.noreply.github.com>
2022-07-19 18:59:45 +0700

    split into class

---
+++ daisy_application/lib/app/pages/portfolio/view/portfolio_artworks.dart
@@ -0,0 +1,8 @@
+import 'package:daisy_application/app/pages/portfolio/view/portfolio.dart';
+import 'package:flutter/material.dart';
+
+extension PortfolioArtworks on PortfolioPageState {
+  Widget createPortfolioTab() {
+    return const Text('Portfolio');
+  }
+}
