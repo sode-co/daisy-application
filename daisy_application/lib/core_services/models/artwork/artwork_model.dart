3adef2d9b97b5f21d179b766295004142da21ba6
= <54256540+ngocthieuff@users.noreply.github.com>
2022-07-20 00:01:48 +0700

    add artwork model

---
+++ daisy_application/lib/core_services/models/artwork/artwork_model.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:daisy_application/core_services/models/category/category_model.dart';
+import 'package:daisy_application/core_services/models/portfolio/portfolio_model.dart';
+import 'package:hive/hive.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'artwork_model.g.dart';
+
+@JsonSerializable()
+class ArtworkModel extends JsonSerializable with HiveObjectMixin {
+  int? id;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  DateTime? deletedAt;
+  String? description;
+  String? image;
+  PortfolioModel? portfolio;
+  CategoryModel? category;
+  String? title;
+  String? data;
+
+  ArtworkModel({
+    this.id,
+    this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+    this.description,
+    this.image,
+    this.portfolio,
+    this.category,
+    this.title,
+    this.data,
+  });
+  ArtworkModel.init();
+
+  factory ArtworkModel.fromJson(Map<String, dynamic> json) =>
+      _$ArtworkModelFromJson(json);
+  @override
+  Map<String, dynamic> toJson() => _$ArtworkModelToJson(this);
+}
---
+++ daisy_application/lib/core_services/models/artwork/artwork_model.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'artwork_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ArtworkModel _$ArtworkModelFromJson(Map<String, dynamic> json) => ArtworkModel(
+      id: json['id'] as int?,
+      createdAt: json['createdAt'] == null
+          ? null
+          : DateTime.parse(json['createdAt'] as String),
+      updatedAt: json['updatedAt'] == null
+          ? null
+          : DateTime.parse(json['updatedAt'] as String),
+      deletedAt: json['deletedAt'] == null
+          ? null
+          : DateTime.parse(json['deletedAt'] as String),
+      description: json['description'] as String?,
+      image: json['image'] as String?,
+      portfolio: json['portfolio'] == null
+          ? null
+          : PortfolioModel.fromJson(json['portfolio'] as Map<String, dynamic>),
+      category: json['category'] == null
+          ? null
+          : CategoryModel.fromJson(json['category'] as Map<String, dynamic>),
+      title: json['title'] as String?,
+      data: json['data'] as String?,
+    );
+
+Map<String, dynamic> _$ArtworkModelToJson(ArtworkModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'createdAt': instance.createdAt?.toIso8601String(),
+      'updatedAt': instance.updatedAt?.toIso8601String(),
+      'deletedAt': instance.deletedAt?.toIso8601String(),
+      'description': instance.description,
+      'image': instance.image,
+      'portfolio': instance.portfolio,
+      'category': instance.category,
+      'title': instance.title,
+      'data': instance.data,
+    };
